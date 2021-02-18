e2f0ba36fc38864b8372edbd8775867226d23c52
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-02-18 20:11:12 -0300

    el listado es consumido desde la DB

--- app/src/main/java/cl/eme/pokemonesreloaded/data/Repository.kt
+++ app/src/main/java/cl/eme/pokemonesreloaded/data/Repository.kt
@@ -3,30 +3,47 @@ package cl.eme.pokemonesreloaded.data
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import cl.eme.pokemonesreloaded.data.db.PokeApplication
+import cl.eme.pokemonesreloaded.data.db.PokemonEntity
 import cl.eme.pokemonesreloaded.data.pojo.Pokemon
 import cl.eme.pokemonesreloaded.data.pojo.PokemonDetail
 import cl.eme.pokemonesreloaded.data.remote.RetrofitClient
 
 class Repository {
 
-    private val pokeList = MutableLiveData<List<Pokemon>>()
-    fun pokelist(): LiveData<List<Pokemon>> = pokeList
+    private val pokeDB = PokeApplication.pokeDb!!.pokeDao()
+
+    fun pokelist(): LiveData<List<Pokemon>> = Transformations.map(pokeDB.getPokemones()) {
+        it.map { entity -> db2api(entity) }
+    }
 
     private val detail = MutableLiveData<PokemonDetail>()
     fun getDetail(): LiveData<PokemonDetail> = detail
 
     suspend fun getPokemones() {
-
         val response = RetrofitClient.retrofitInstance().getPokemones()
-
-        Log.d("PokeViewModel", "cargando info")
 
         response.let {
             when (it.isSuccessful) {
-                true -> pokeList.value = response.body()
+                true -> {
+                    response.body()?.let { pokeList ->
+                        val map = pokeList.map { poke -> api2db(poke) }
+                        pokeDB.insertPokemones(map)
+                    }
+
+                }
                 false -> Log.d("PokeViewModel", "epa! error")
             }
         }
+    }
+
+    private fun api2db(pokemon: Pokemon): PokemonEntity {
+        return PokemonEntity(pokemon.id, pokemon.name, pokemon.img)
+    }
+
+    private fun db2api(pokemonEntity: PokemonEntity): Pokemon {
+        return Pokemon(pokemonEntity.id, pokemonEntity.name, pokemonEntity.img)
     }
 
     suspend fun getDetail(id: String) {
